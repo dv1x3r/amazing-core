dd650b34dd6332c8346513f1a3af2ab3ff77daf0
DV1X3R <d.purvinsh@gmail.com>
2021-06-18 01:05:17 +0300

    cached files extractor init

---
+++ tools/assets_extractor.ps1
@@ -0,0 +1,8 @@
+﻿$ripper = "C:\Users\dx\source\repos\UtinyRipper-master\Bins\CP\Debug\uTinyRipperCmd.dll"
+$src_dir = "C:\Users\dx\source\repos\UtinyRipper-master\Bins\CP\Debug\Cache\"
+$target_dir = "C:\Users\dx\source\repos\UtinyRipper-master\Bins\CP\Debug\"
+cd $target_dir
+
+Get-ChildItem $src_dir | Foreach-Object {
+    dotnet $ripper $_.FullName
+}
