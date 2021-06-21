c5d0e5da0e50b83acb714ddf3e4669ff329bd3d0
DV1X3R <d.purvinsh@gmail.com>
2021-06-21 23:46:22 +0300

    app logo and link

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import asyncio
 from amazingcore.core import Core
 from amazingcdn.cdn import Cdn
 
+small_slant = '''
+           ___   __  ______ ____  _____  _______
+          / _ | /  |/  / _ /_  / /  _/ |/ / ___/
+         / __ |/ /|_/ / __ |/ /__/ //    / (_ / 
+        /_/ |_/_/  /_/_/ |_/___/___/_/|_/\___/  
+                        / ___/ __ \/ _ \/ __/   
+                       / /__/ /_/ / , _/ _/     
+                       \___/\____/_/|_/___/     
+  AmazingCore 0.0.1 - github.com/dv1x3r/amazing-core
+'''
+print(small_slant)
+
 
 async def main():
     tasks = [Core().main('127.0.0.1', 8182), Cdn().main('127.0.0.1', 8080)]
