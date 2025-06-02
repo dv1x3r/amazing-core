33fce0898c2ca6e1482342335d917cd984028d44
dv1x3r <d.purvinsh@gmail.com>
2025-06-02 13:34:49 +0300

    add analyzer magic signatures

--- tools/cache-analyzer/main.go
+++ tools/cache-analyzer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"flag"
 	"fmt"
@@ -50,6 +51,29 @@ func analyzeBlobDB(blobDB string) error {
 }
 
 func processRow(cdnid string, blob []byte) error {
-	fmt.Println(cdnid)
+	fmt.Printf("cdnid=%s type=%s version=%s \n", cdnid, detectFileType(blob), "")
 	return nil
+}
+
+func hasPrefix(blob, prefix []byte) bool {
+	return len(blob) >= len(prefix) && bytes.Equal(blob[:len(prefix)], prefix)
+}
+
+func detectFileType(blob []byte) string {
+	switch {
+	case hasPrefix(blob, []byte{0x7b}):
+		return "json"
+	case hasPrefix(blob, []byte{0x4F, 0x67, 0x67, 0x53, 0x00}):
+		return "audio/ogg"
+	case hasPrefix(blob, []byte{0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00}):
+		return "image/png"
+	case hasPrefix(blob, []byte{0x55, 0x6E, 0x69, 0x74, 0x79, 0x57, 0x65, 0x62, 0x00}):
+		return "UnityWeb"
+	case hasPrefix(blob, []byte{0x55, 0x6E, 0x69, 0x74, 0x79, 0x46, 0x53, 0x00}):
+		return "UnityFS"
+	case hasPrefix(blob, []byte{0x51, 0x75, 0x65, 0x73, 0x74, 0x0D, 0x0A, 0x7B}):
+		return "TreeNode/Quest"
+	default:
+		return ""
+	}
 }
