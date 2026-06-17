8acb9d252b06524bb0bf5f93b894cf1a6c8a0e2d
dv1x3r <d.purvinsh@gmail.com>
2026-06-18 01:56:44 +0300

    log message headers for info

--- internal/game/middleware/logger.go
+++ internal/game/middleware/logger.go
@@ -31,20 +31,19 @@ func Logger(logger *slog.Logger) gsf.Middleware {
 				slog.String("remote_ip", r.RemoteIP()),
 				slog.String("platform", r.Platform().String()),
 				slog.Int("result_code", int(w.Header().ResultCode)),
+				slog.String("app_code_text", w.Header().AppCodeText()),
 				slog.Int("app_code", int(w.Header().AppCode)),
 				slog.Int("request_id", int(r.Header().RequestID)),
 				slog.Int("req_flags", int(r.Header().Flags)),
 				slog.Int("res_flags", int(w.Header().Flags)),
 				slog.Int("svc_class", int(r.Header().SvcClass)),
+				slog.String("svc_class_text", r.Header().ServiceClassText()),
 				slog.Int("msg_type", int(r.Header().MsgType)),
+				slog.String("msg_type_text", r.Header().MessageTypeText()),
 				slog.String("latency", latency),
 			}
 
 			if debug {
-				attrs = append(attrs, slog.String("result_code_text", w.Header().ResultCodeText()))
-				attrs = append(attrs, slog.String("app_code_text", w.Header().AppCodeText()))
-				attrs = append(attrs, slog.String("svc_class_text", r.Header().ServiceClassText()))
-				attrs = append(attrs, slog.String("msg_type_text", r.Header().MessageTypeText()))
 				attrs = append(attrs, slog.Any("request", r.Body()))
 				attrs = append(attrs, slog.Any("response", w.Body()))
 			}
